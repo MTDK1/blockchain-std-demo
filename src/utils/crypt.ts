4ae80a75649518c0b6b39d1762f02b12651670a4
Koichi Matsuda <k-matsuda@caldia.org>
2019-11-25 12:52:14 +0900

    戻りの型を追加

---
+++ src/utils/crypt.ts
@@ -0,0 +1,15 @@
+// 暗号化に関するユーティリティ
+
+export enum CryptTypes {
+  SHA256,
+}
+
+export interface Crypt {
+  
+}
+
+
+// 外部ライブラリをラップしたものを作成する
+
+
+// 呼び出すためのファクトり
--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,8 +1,6 @@
-const debug = require('debug')
-
-export function logFactory(name: string) {
-  if (!name || name.length === 0) {
-    name = '[noname]';
-  }
-  return debug(name);
+export function logFactory(
+  name: string = 'DEBUG'
+): (params: any, ...optionparams: any) => void {
+  const debug = require('debug')
+  return debug(name)
 }
