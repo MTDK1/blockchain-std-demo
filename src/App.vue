5314a914a76c97c2a4a841c423c3e2ac3ab417a7
Koichi Matsuda <k-matsuda@caldia.org>
2019-11-26 17:56:44 +0900

    ロゴ、トップページ修正

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
 
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>BDevUx Demo</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
