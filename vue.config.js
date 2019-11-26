9adc337b213a1dad1be1b9bebf575a289b92417f
Koichi Matsuda <k-matsuda@caldia.org>
2019-11-26 17:02:45 +0900

    base url 修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: "/",
+  publicPath: process.env.NODE_ENV === 'production'
+  ? 'blockchain-std-demo/'
+  : '/',
   assetsDir: "./",
   transpileDependencies: ["vuetify"]
 };
