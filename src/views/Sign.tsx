4068434a40a340e9889c42355f92ca195f7dd153
Koichi Matsuda <k-matsuda@caldia.org>
2019-12-20 09:49:48 +0900

    カードを追加

--- src/views/Sign.tsx
+++ src/views/Sign.tsx
@@ -13,6 +13,13 @@ export class Sign extends Vue {
       <div>
         <MessageCard title="署名作成と検証"></MessageCard>
         <PrivateKey class="mt-5" title="秘密鍵" />
+        <MessageCard class="mt-5" title="署名する情報を入力"></MessageCard>
+        <MessageCard class="mt-5" title="ハッシュ値を計算"></MessageCard>
+        <MessageCard class="mt-5" title="秘密鍵を使ってハッシュ値を暗号化"></MessageCard>
+        <MessageCard class="mt-5" title="情報を送信（署名と公開鍵付）"></MessageCard>
+        <MessageCard class="mt-5" title="受け取った情報からハッシュ値を計算"></MessageCard>
+        <MessageCard class="mt-5" title="受け取った署名を公開鍵を使って復号化"></MessageCard>
+        <MessageCard class="mt-5" title="計算したハッシュ値と復号化した値を比較"></MessageCard>
       </div>
     );
   }
